fa8567009e34e9b7ee5cba14b7059402d29c7685
hanna <hanna.kalashnik@gmail.com>
2020-08-12 10:03:51 +0300

    commit for github - AWS pipeline

--- src/main/java/com/rest/Application.java
+++ src/main/java/com/rest/Application.java
@@ -7,7 +7,6 @@ import com.rest.VaadinUI.MainView;
 import com.rest.services.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
